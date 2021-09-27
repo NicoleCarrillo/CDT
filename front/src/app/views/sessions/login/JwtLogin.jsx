322d6954449e819bcaa49e0ea4d35413c6a395f2
Nicole Carrillo C <A01551581@itesm.mx>
2021-09-27 02:26:36 -0500

    init page ready

--- front/src/app/views/sessions/login/JwtLogin.jsx
+++ front/src/app/views/sessions/login/JwtLogin.jsx
@@ -1,27 +1,25 @@
 import React, { useState } from 'react'
 import {
     Card,
-    Checkbox,
-    FormControlLabel,
     Grid,
     Button,
     CircularProgress,
 } from '@material-ui/core'
-import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator'
+import Box from '@material-ui/core/Box';
 
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles,withStyles } from '@material-ui/core/styles'
 import history from 'history.js'
 import clsx from 'clsx'
 import useAuth from 'app/hooks/useAuth'
 
 const useStyles = makeStyles(({ palette, ...theme }) => ({
     cardHolder: {
-        background: '#1A2038',
+        background: '#3D372F',
     },
     card: {
-        maxWidth: 800,
-        borderRadius: 12,
-        margin: '1rem',
+        maxWidth: 700,
+        borderRadius: 8,
+        margin: '3rem',
     },
     buttonProgress: {
         position: 'absolute',
@@ -34,25 +32,12 @@ const useStyles = makeStyles(({ palette, ...theme }) => ({
 
 const JwtLogin = () => {
     const [loading, setLoading] = useState(false)
-    const [userInfo, setUserInfo] = useState({
-        email: 'jason@ui-lib.com',
-        password: 'dummyPass',
-    })
     const [message, setMessage] = useState('')
-    const { login } = useAuth()
-
     const classes = useStyles()
-
-    const handleChange = ({ target: { name, value } }) => {
-        let temp = { ...userInfo }
-        temp[name] = value
-        setUserInfo(temp)
-    }
 
     const handleFormSubmit = async (event) => {
         setLoading(true)
         try {
-            await login(userInfo.email, userInfo.password)
             history.push('/')
         } catch (e) {
             console.log(e)
@@ -61,122 +46,49 @@ const JwtLogin = () => {
         }
     }
 
+    const ColorButton = withStyles((theme) => ({
+        root: {
+          color: "#ffffff",
+          fontSize: 10,
+          fontWeight:700,
+          backgroundColor: "#616060",
+          '&:hover': {
+            backgroundColor: "#3D372F",
+          },
+        },
+      }))(Button);
+
+
     return (
         <div
             className={clsx(
                 'flex justify-center items-center  min-h-full-screen',
                 classes.cardHolder
             )}
-        >
+            >
             <Card className={classes.card}>
                 <Grid container>
-                    <Grid item lg={5} md={5} sm={5} xs={12}>
-                        <div className="p-8 flex justify-center items-center h-full">
-                            <img
-                                className="w-200"
-                                src="/assets/images/illustrations/dreamer.svg"
-                                alt=""
-                            />
-                        </div>
-                    </Grid>
-                    <Grid item lg={7} md={7} sm={7} xs={12}>
-                        <div className="p-8 h-full bg-light-gray relative">
-                            <ValidatorForm onSubmit={handleFormSubmit}>
-                                <TextValidator
-                                    className="mb-6 w-full"
-                                    variant="outlined"
-                                    size="small"
-                                    label="Email"
-                                    onChange={handleChange}
-                                    type="email"
-                                    name="email"
-                                    value={userInfo.email}
-                                    validators={['required', 'isEmail']}
-                                    errorMessages={[
-                                        'this field is required',
-                                        'email is not valid',
-                                    ]}
-                                />
-                                <TextValidator
-                                    className="mb-3 w-full"
-                                    label="Password"
-                                    variant="outlined"
-                                    size="small"
-                                    onChange={handleChange}
-                                    name="password"
-                                    type="password"
-                                    value={userInfo.password}
-                                    validators={['required']}
-                                    errorMessages={['this field is required']}
-                                />
-                                <FormControlLabel
-                                    className="mb-3 min-w-288"
-                                    name="agreement"
-                                    onChange={handleChange}
-                                    control={
-                                        <Checkbox
-                                            size="small"
-                                            onChange={({
-                                                target: { checked },
-                                            }) =>
-                                                handleChange({
-                                                    target: {
-                                                        name: 'agreement',
-                                                        value: checked,
-                                                    },
-                                                })
-                                            }
-                                            checked={userInfo.agreement || true}
-                                        />
-                                    }
-                                    label="Remeber me"
-                                />
-
-                                {message && (
-                                    <p className="text-error">{message}</p>
-                                )}
-
-                                <div className="flex flex-wrap items-center mb-4">
-                                    <div className="relative">
-                                        <Button
-                                            variant="contained"
-                                            color="primary"
-                                            disabled={loading}
-                                            type="submit"
-                                        >
-                                            Sign in
-                                        </Button>
-                                        {loading && (
-                                            <CircularProgress
-                                                size={24}
-                                                className={
-                                                    classes.buttonProgress
-                                                }
-                                            />
-                                        )}
-                                    </div>
-                                    <span className="mr-2 ml-5">or</span>
-                                    <Button
-                                        className="capitalize"
-                                        onClick={() =>
-                                            history.push('/session/signup')
-                                        }
-                                    >
-                                        Sign up
-                                    </Button>
-                                </div>
-                                <Button
-                                    className="text-primary"
-                                    onClick={() =>
-                                        history.push('/session/forgot-password')
-                                    }
-                                >
-                                    Forgot password?
-                                </Button>
-                            </ValidatorForm>
-                        </div>
+                    <Grid item lg={12} md={12} sm={12} xs={12}>
+                        <img
+                            class="w-300 justify-center h-full"
+                            src="/assets/images/logos/LogoCDT.png"
+                            text-align= "center"
+                        />
                     </Grid>
                 </Grid>
+                <Box textAlign='center' marginTop='20px' marginBottom='10px'>
+                    <ColorButton variant="contained" size="small" disabled={loading} type="submit" onClick={handleFormSubmit}>
+                        Comenzar
+                    </ColorButton>
+                    {loading && (
+                        <CircularProgress
+                            size={24}
+                            className={
+                                classes.buttonProgress
+                            }
+                        />
+                    )}
+                </Box>
             </Card>
         </div>
     )
